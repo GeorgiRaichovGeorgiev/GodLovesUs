6ce37ee8c50a4b65d34218c3dbdb772af0b945bd
Georgi Georgiev <georgiraichovgeorgiev@gmail.com>
2019-07-13 16:20:52 +0300

    Test 'Fragment' and 'createRef() - new ref syntax'

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
 import '../scss/main.scss';
 
-const Questions = () => {
-    const questions = [
-        {
-            pk: 1,
-            question_title: 'Какво правиш?',
-        },
-        {
-            pk: 2,
-            question_title: 'Как си?',
-        },
-    ];
+export class Questions extends React.Component {
+    constructor(props) {
+        super(props);
 
-    return (
-        <div>
-            {questions.map(question =>
-                <Question key={question.pk} questionTitle={question.question_title}/>
-            )}
-        </div>
-    );
+        // new ref syntax
+        this.wrapperRef = React.createRef();
+        this.questionRef = React.createRef();
+    }
+
+    // class fields - method definition (without bounding)
+    handleClickQuestionTitle = () => {
+        console.log(this.wrapperRef.current.className);
+        console.log(this.questionRef.current.imQuestion());
+    };
+
+    render() {
+        return (
+            <div className="questions-wrapper" ref={this.wrapperRef}>
+                {this.props.questions.map(question =>
+                    <Question
+                        key={question.pk}
+                        questionTitle={question.question_title}
+                        handleClickQuestionTitle={this.handleClickQuestionTitle}
+                        ref={this.questionRef}
+                    />
+                )}
+            </div>
+        );
+    }
+}
+
+// propTypes validation
+Questions.propTypes = {
+    questions: PropTypes.array.isRequired,
 };
-
 
 
 export class Question extends React.Component {
@@ -31,6 +45,9 @@ export class Question extends React.Component {
     state = {
         counter: 0,
     };
+
+    // class fields - method definition (without bounding)
+    imQuestion = () => "I'm a Question component!";
 
     // class fields - method definition (without bounding)
     incrementCounter = () => {
@@ -44,7 +61,11 @@ export class Question extends React.Component {
     render() {
         return (
             <li onClick={this.incrementCounter}>
-                Въпрос: <QuestionTitle name={this.props.questionTitle}/>
+                Въпрос:
+                <QuestionTitle
+                    name={this.props.questionTitle}
+                    handleClickQuestionTitle={this.props.handleClickQuestionTitle}
+                />
             </li>
         );
     }
@@ -52,20 +73,28 @@ export class Question extends React.Component {
 
 // propTypes validation
 Question.propTypes = {
-    questionTitle: PropTypes.string,
+    questionTitle: PropTypes.string.isRequired,
+    handleClickQuestionTitle: PropTypes.func,
 };
 
 
 
 // Destructing props value
-const QuestionTitle = ({name, state}) => {
-    return <span>{name} ({state})</span>;
+const QuestionTitle = ({name, state, handleClickQuestionTitle}) => {
+    return (
+        // Fragment
+        <Fragment>
+            <span onClick={handleClickQuestionTitle}>{name}</span>
+            <span>({state})</span>
+        </Fragment>
+    );
 };
 
 // propTypes validation
 QuestionTitle.propTypes = {
     name: PropTypes.string.isRequired,
     state: PropTypes.string.isRequired,
+    handleClickQuestionTitle: PropTypes.func,
 };
 
 // defaultProps
@@ -74,4 +103,19 @@ QuestionTitle.defaultProps = {
 };
 
 
-ReactDOM.render(<Questions/>, document.getElementById('react'));
+
+const questionsData = [
+    {
+        pk: 1,
+        question_title: 'Какво правиш?',
+    },
+    {
+        pk: 2,
+        question_title: 'Как си?',
+    },
+];
+
+ReactDOM.render(
+    <Questions questions={questionsData}/>,
+    document.getElementById('react'),
+);
